951589a1f79aad9123a8e0ae99e7d19e3c9f94fc
Mehul Gupta <mehulgupta2110@gmail.com>
2023-08-05 23:59:25 +0530

    Hii

---
+++ Goal-Tracker/App.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
+
+export default function App() {
+  const [goal, updateGoal] = useState("");
+  const [list, updateList] = useState([]);
+
+  const addGoalHandler = () => {
+    updateList((list) => [...list, goal]);
+  };
+
+  const goalInputHandler = (enteredText) => {
+    updateGoal(enteredText);
+  };
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.listContainer}>
+      <ScrollView >
+        {list.map((items) => (
+          <Text key={Math.random().toString()} style={styles.individualGoal}>
+            {items}
+          </Text>
+        ))}
+      </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  listContainer: {
+    flex: 6,
+    margin: 16,
+  },
+  individualGoal: {
+    fontSize: 16,
+    padding: 8,
+    borderRadius: 8,
+    color: "white",
+    marginBottom: 16,
+    backgroundColor: "#5e0acc",
+  },
+});
